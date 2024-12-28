7b24c4bbe2d286faaa311743d96b589264397051
ReetikaNEU <bhanushali.re@northeastern.edu>
2024-12-27 22:55:19 -0500

    setting up the base code

--- test_script.py
+++ test_script.py
@@ -1,3 +1,7 @@
 def sample_function():
+    """A sample function that prints a message."""
     print("This is a test function")
-sample_function()
+
+
+if __name__ == "__main__":
+    sample_function()
